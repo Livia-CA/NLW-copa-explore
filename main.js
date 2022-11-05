818fac1e5bec6e414ea2eaaacc25c315a21b6fe6
Livia Caetano de Andrade <liviacaetanodeandrade@gmail.com>
2022-11-05 12:14:21 -0400

    adiciona animações, CSS variables e refatora código

--- main.js
+++ main.js
@@ -7,10 +7,11 @@ function createGame(player1, hour, player2) {
         </li>
     `
 }
-
+let delay = -0.3;
 function createCard(date, day, games) {
+    delay = delay + 0.3;
     return `
-        <div class="card">
+        <div class="card" style="animation-delay: ${delay}s">
             <h2>${date}<span>${day}</span></h2>
 
             <ul>
@@ -20,13 +21,7 @@ function createCard(date, day, games) {
     `
 }
 
-document.querySelector("#app").innerHTML = `
-    <header>
-        <img src="./assets/logo.svg" alt="logo da NLW">
-    </header>
+document.querySelector("#cards").innerHTML = 
+        createCard('24/11','quinta', createGame('brazil', '16:00', 'serbia') + createGame('brazil', '16:00', 'serbia')) +
+        createCard('28/11','sexta', createGame('brazil', '13:00', 'switzerland'));
 
-    <main id="cards">
-        ${createCard('24/11','quinta', createGame('brazil', '16:00', 'serbia') + createGame('brazil', '16:00', 'serbia'))}
-        ${createCard('28/11','sexta', createGame('brazil', '13:00', 'switzerland'))}
-    </main>
-`
